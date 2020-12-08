155d9aff4306c0f6325fbe402dc273f6ab6ddb98
Vishal Bihani <67241951+vishalbihani1306@users.noreply.github.com>
2020-12-08 21:30:06 +0530

    Add files via upload

---
+++ tingo_curl.sh
@@ -0,0 +1,4 @@
+curl https://api.tiingo.com/tiingo/daily/GOOGL/prices/?startDate=2020-05-01/&endDate=2020-05-05/&token=ba4f9b6f7ae38f0aef613a47ec5f218ba5206b8b
+curl https://api.tiingo.com/tiingo/daily/AAPL/prices/?startDate=2020-05-01/&endDate=2020-05-05/&token=ba4f9b6f7ae38f0aef613a47ec5f218ba5206b8b
+curl https://api.tiingo.com/tiingo/daily/MSFT/prices/?startDate=2020-05-01/&endDate=2020-05-05/&token=ba4f9b6f7ae38f0aef613a47ec5f218ba5206b8b
+
